8ba576af2569633d1dd999a62174d7d63a76a8e3
b33456386-cmd <b33456386@gmail.com>
2026-02-25 23:47:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import requests
 
-# 🔑 تنظیمات
 BOT_TOKEN = "8551688721:AAHyFlOL5WZYjgAuswz81X_SCi898k1DOUM"
 CHAT_ID = "@jdkdjjdjkf"
 
-# 📤 ارسال پیام
 def send_message(text):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     data = {
@@ -14,9 +12,8 @@ def send_message(text):
     response = requests.post(url, data=data)
     print(response.text)
 
-# 🚀 اجرای اصلی
 def main():
-    send_message("🔥 ربات با موفقیت وصل شد و داره کار می‌کنه!")
+    send_message("🔥 ربات با موفقیت کار کرد!")
 
 if name == "main":
     main()
