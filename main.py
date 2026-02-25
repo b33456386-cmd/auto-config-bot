d3389736832df621a9274ccfb2e50dd21806ad8f
b33456386-cmd <b33456386@gmail.com>
2026-02-25 23:57:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-import os
 import requests
+import os
 
-TOKEN = os.getenv("8551688721:AAHyFlOL5WZYjgAuswz81X_SCi898k1DOUM")
+BOT_TOKEN = os.getenv("8551688721:AAHyFlOL5WZYjgAuswz81X_SCi898k1DOUM")
 CHAT_ID = os.getenv("@jdkdjjdjkf")
 
-def send_message(text):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+def main():
+    text = "ربات تست شد ✅"
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     data = {
         "chat_id": CHAT_ID,
         "text": text
     }
-    response = requests.post(url, data=data)
-    print(response.text)
-
-def main():
-    print("Bot started...")
-    send_message("✅ ربات با موفقیت اجرا شد!")
+    requests.post(url, data=data)
 
 if name == "main":
     main()
